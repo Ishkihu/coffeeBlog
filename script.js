a1996d04c2fa25dd4ad06843b228dc684b79fdde
JV-Esquejo <esquejojohnvictor@gmail.com>
2024-11-09 11:53:24 +0800

    Multiple Commits
    forgot to commit regularly

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('section-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
